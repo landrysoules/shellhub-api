0ff6762e85fe24abe28f62302ac2771138eae20c
Landry Soules <landry.soules@gmail.com>
2015-05-10 19:18:08 +0200

    Fixed problem with rack.

--- app/shellhub.rb
+++ app/shellhub.rb
@@ -1,12 +1,15 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'json'
-post '/run' do
-  content_type :json
-  data = JSON.parse( request.body.read.to_s )
-  command = data["command"]
-  result = []
-  IO.popen(["bash","-c",command]).each do |line|
-    result << line.chomp
+
+class ShellHubApi < Sinatra::Base
+  post '/run' do
+    content_type :json
+    data = JSON.parse( request.body.read.to_s )
+    command = data["command"]
+    result = []
+    IO.popen(["bash","-c",command]).each do |line|
+      result << line.chomp
+    end
+    {command: command, result: result}.to_json
   end
-  {command: command, result: result}.to_json
 end
--- config.ru
+++ config.ru
@@ -1,3 +1,7 @@
 require "./config/environment"
+require "./app/shellhub"
 
-run Rack::URLMap.new("/" => ShellHubApi)
+#run Rack::URLMap.new("/" => ShellHubApi)
+
+run ShellHubApi.new
+
