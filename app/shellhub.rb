4508ad2ad280dca7313a1d6c34b495a2b8a54e36
Landry Soules <landry.soules@gmail.com>
2015-05-10 23:12:45 +0200

    added CORS support.

--- app/shellhub.rb
+++ app/shellhub.rb
@@ -2,7 +2,8 @@ require 'sinatra/base'
 require 'json'
 
 class ShellHubApi < Sinatra::Base
-  post '/run' do
+  post '/run' do 
+    response['Access-Control-Allow-Origin'] = '*'
     content_type :json
     data = JSON.parse( request.body.read.to_s )
     command = data["command"]
